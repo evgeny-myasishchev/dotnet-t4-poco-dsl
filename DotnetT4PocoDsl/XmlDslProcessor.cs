2d5fecb1fd796bd9ce49267ed60f050bfdd93459
Evgeny Myasishchev <evgeny.myasishchev@gmail.com>
2015-10-29 12:28:08 +0200

    Code cleanup.

--- DotnetT4PocoDsl/XmlDslProcessor.cs
+++ DotnetT4PocoDsl/XmlDslProcessor.cs
@@ -1,26 +1,22 @@
 ﻿//Generated file. Do not edit it manually.
 using System;
 
-namespace DotnetT4PocoDsl
+namespace DotnetT4PocoDsl.Commands
 {
-    public partial class AddDepartment : IEquatable<AddDepartment>
+    public class AddDepartmentCommand : IEquatable<AddDepartmentCommand>
     {
         public string ReferenceNumber { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public AddDepartment()
+        public AddDepartmentCommand(string referencenumber = default(string), string name = default(string), string description = default(string))
         {
+            ReferenceNumber = referencenumber;
+            Name = name;
+            Description = description;
         }
 
-        public AddDepartment(string aReferenceNumber = default(string),string aName = default(string),string aDescription = default(string))
-        {
-            ReferenceNumber = aReferenceNumber;
-            Name = aName;
-            Description = aDescription;
-        }
-
-        public bool Equals(AddDepartment other)
+        public bool Equals(AddDepartmentCommand other)
         {
             return
                 ReferenceNumber == other.ReferenceNumber &&
@@ -30,30 +26,80 @@ namespace DotnetT4PocoDsl
 
         public override string ToString()
         {
-            return string.Format("<AddDepartment ReferenceNumber: {0}, Name: {1}, Description: {2}>", ReferenceNumber, Name, Description);
+            return string.Format("<AddDepartmentCommand ReferenceNumber: {0}, Name: {1}, Description: {2}>", ReferenceNumber, Name, Description);
         }
     }
 
-    public partial class UpdateDepartment : IEquatable<UpdateDepartment>
+    public class AddDepartmentCommand1 : IEquatable<AddDepartmentCommand1>
+    {
+        public string ReferenceNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public AddDepartmentCommand1(string referencenumber = default(string), string name = default(string), string description = default(string))
+        {
+            ReferenceNumber = referencenumber;
+            Name = name;
+            Description = description;
+        }
+
+        public bool Equals(AddDepartmentCommand1 other)
+        {
+            return
+                ReferenceNumber == other.ReferenceNumber &&
+                Name == other.Name &&
+                Description == other.Description;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("<AddDepartmentCommand1 ReferenceNumber: {0}, Name: {1}, Description: {2}>", ReferenceNumber, Name, Description);
+        }
+    }
+
+    public class AddDepartmentCommand2 : IEquatable<AddDepartmentCommand2>
+    {
+        public string ReferenceNumber { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+        public AddDepartmentCommand2(string referencenumber = default(string), string name = default(string), string description = default(string))
+        {
+            ReferenceNumber = referencenumber;
+            Name = name;
+            Description = description;
+        }
+
+        public bool Equals(AddDepartmentCommand2 other)
+        {
+            return
+                ReferenceNumber == other.ReferenceNumber &&
+                Name == other.Name &&
+                Description == other.Description;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("<AddDepartmentCommand2 ReferenceNumber: {0}, Name: {1}, Description: {2}>", ReferenceNumber, Name, Description);
+        }
+    }
+
+    public class UpdateDepartmentCommand : IEquatable<UpdateDepartmentCommand>
     {
         public int Id { get; set; }
         public string ReferenceNumber { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public UpdateDepartment()
+        public UpdateDepartmentCommand(int id = default(int), string referencenumber = default(string), string name = default(string), string description = default(string))
         {
+            Id = id;
+            ReferenceNumber = referencenumber;
+            Name = name;
+            Description = description;
         }
 
-        public UpdateDepartment(int aId = default(int),string aReferenceNumber = default(string),string aName = default(string),string aDescription = default(string))
-        {
-            Id = aId;
-            ReferenceNumber = aReferenceNumber;
-            Name = aName;
-            Description = aDescription;
-        }
-
-        public bool Equals(UpdateDepartment other)
+        public bool Equals(UpdateDepartmentCommand other)
         {
             return
                 Id == other.Id &&
@@ -64,26 +110,22 @@ namespace DotnetT4PocoDsl
 
         public override string ToString()
         {
-            return string.Format("<UpdateDepartment Id: {0}, ReferenceNumber: {1}, Name: {2}, Description: {3}>", Id, ReferenceNumber, Name, Description);
+            return string.Format("<UpdateDepartmentCommand Id: {0}, ReferenceNumber: {1}, Name: {2}, Description: {3}>", Id, ReferenceNumber, Name, Description);
         }
     }
 
-    public partial class RemoveDepartment : IEquatable<RemoveDepartment>
+    public class RemoveDepartmentCommand : IEquatable<RemoveDepartmentCommand>
     {
         public int Id { get; set; }
         public string Reason { get; set; }
 
-        public RemoveDepartment()
+        public RemoveDepartmentCommand(int id = default(int), string reason = default(string))
         {
+            Id = id;
+            Reason = reason;
         }
 
-        public RemoveDepartment(int aId = default(int),string aReason = default(string))
-        {
-            Id = aId;
-            Reason = aReason;
-        }
-
-        public bool Equals(RemoveDepartment other)
+        public bool Equals(RemoveDepartmentCommand other)
         {
             return
                 Id == other.Id &&
@@ -92,29 +134,25 @@ namespace DotnetT4PocoDsl
 
         public override string ToString()
         {
-            return string.Format("<RemoveDepartment Id: {0}, Reason: {1}>", Id, Reason);
+            return string.Format("<RemoveDepartmentCommand Id: {0}, Reason: {1}>", Id, Reason);
         }
     }
 }
-namespace DotnetT4PocoDsl
+namespace DotnetT4PocoDsl.Events
 {
-    public partial class DepartmentAdded : IEquatable<DepartmentAdded>
+    public class DepartmentAdded : IEquatable<DepartmentAdded>
     {
         public int Id { get; set; }
         public string ReferenceNumber { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public DepartmentAdded()
+        public DepartmentAdded(int id = default(int), string referencenumber = default(string), string name = default(string), string description = default(string))
         {
-        }
-
-        public DepartmentAdded(int aId = default(int),string aReferenceNumber = default(string),string aName = default(string),string aDescription = default(string))
-        {
-            Id = aId;
-            ReferenceNumber = aReferenceNumber;
-            Name = aName;
-            Description = aDescription;
+            Id = id;
+            ReferenceNumber = referencenumber;
+            Name = name;
+            Description = description;
         }
 
         public bool Equals(DepartmentAdded other)
@@ -132,23 +170,19 @@ namespace DotnetT4PocoDsl
         }
     }
 
-    public partial class DepartmentUpdated : IEquatable<DepartmentUpdated>
+    public class DepartmentUpdated : IEquatable<DepartmentUpdated>
     {
         public int Id { get; set; }
         public string ReferenceNumber { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
-        public DepartmentUpdated()
+        public DepartmentUpdated(int id = default(int), string referencenumber = default(string), string name = default(string), string description = default(string))
         {
-        }
-
-        public DepartmentUpdated(int aId = default(int),string aReferenceNumber = default(string),string aName = default(string),string aDescription = default(string))
-        {
-            Id = aId;
-            ReferenceNumber = aReferenceNumber;
-            Name = aName;
-            Description = aDescription;
+            Id = id;
+            ReferenceNumber = referencenumber;
+            Name = name;
+            Description = description;
         }
 
         public bool Equals(DepartmentUpdated other)
@@ -166,19 +200,15 @@ namespace DotnetT4PocoDsl
         }
     }
 
-    public partial class DepartmentRemoved : IEquatable<DepartmentRemoved>
+    public class DepartmentRemoved : IEquatable<DepartmentRemoved>
     {
         public int Id { get; set; }
         public string Reason { get; set; }
 
-        public DepartmentRemoved()
+        public DepartmentRemoved(int id = default(int), string reason = default(string))
         {
-        }
-
-        public DepartmentRemoved(int aId = default(int),string aReason = default(string))
-        {
-            Id = aId;
-            Reason = aReason;
+            Id = id;
+            Reason = reason;
         }
 
         public bool Equals(DepartmentRemoved other)
